a9fd4138c2d67b69530f9cd2ea7dace1a926c832
amina aksouh <amina10aks@gmail.com>
2025-11-22 12:49:17 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ padding: "40px", fontSize: "24px" }}>
-      Welcome to Fish Your Style  🎣💙
+      Welcome to Fish Your Style  hey 🎣💙
     </div>
   );
 }
