c696ee61ccf731417659ac78761aa77b9f5a3850
amina aks <171484522+amina23aks@users.noreply.github.com>
2025-12-01 17:13:18 +0100

    Restore page scrolling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ocean-page relative min-h-screen overflow-hidden antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ocean-page relative min-h-screen overflow-x-hidden antialiased`}
       >
         <OceanBackdrop />
         <Navbar />
