3454e73d555a7b6ebb682a92b6a1dbca84bf11ed
amina aks <171484522+amina23aks@users.noreply.github.com>
2026-01-22 15:38:50 +0100

    Add Meta Pixel script to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,8 +54,29 @@ export default function RootLayout({
           type="module"
           strategy="beforeInteractive"
         />
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1217191333852885');
+fbq('track', 'PageView');`}
+        </Script>
       </head>
       <body className="ocean-page relative flex min-h-screen flex-col overflow-x-hidden antialiased font-sans">
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1217191333852885&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <Suspense fallback={null}>
           <AnalyticsProvider>
             <AuthProvider>
